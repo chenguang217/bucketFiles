afbf37b3c58724f9be61d1bc59cb5fa25d186887
chenguang217 <15645456916@163.com>
2021-09-17 10:29:40 +0800

    Update install.py

--- install.py
+++ install.py
@@ -6,37 +6,48 @@ import requests
 
 if __name__ == "__main__":
     os.chdir(sys.argv[1])
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.37'}
-    linkId = sys.argv[2]
-    passId = sys.argv[3]
-    url = 'https://caiyun.139.com/stapi/outlink/info'
-    data = {
-        'linkId': linkId,
-        'path': 'root',
-        'start': 1,
-        'end': 15,
-        'sortType': 0,
-        'sortDr': 1,
-        'pass': passId
-    }
-    session = requests.session()
-    response = session.post(url, headers=headers, data=data)
-    text = response.json()
-    url = 'https://caiyun.139.com/stapi/outlink/content/download'
-    contentIds = text['data']['pCaID'] + '/' + text['data']['coLst']['outLinkCoInfo']['coID']
-    data = {
-        'linkId': linkId,
-        'contentIds': contentIds
-    }
-    response = session.post(url, headers=headers, data=data)
-    text = response.json()
-    link = text['data']['redrUrl']
-    os.system('wget -O tmp.zip "' + link + '"')
-    os.system('7z x tmp.zip')
-    file = rarfile.RarFile('tmp.zip')
-    for f in file.infolist():
-        dir = f.filename.split('/')[0]
-        break
-    os.system('mv ' + dir + '/* ./')
-    os.removedirs(dir)
-    os.remove('tmp.zip')
+    if 'http' in sys.argv[2]:
+        os.system('wget -O tmp.zip "' + sys.argv[2] + '"')
+        os.system('7z x tmp.zip')
+        file = rarfile.RarFile('tmp.zip')
+        for f in file.infolist():
+            dir = f.filename.split('/')[0]
+            break
+        os.system('mv ' + dir + '/* ./')
+        os.removedirs(dir)
+        os.remove('tmp.zip')
+    else:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.37'}
+        linkId = sys.argv[2]
+        passId = sys.argv[3]
+        url = 'https://caiyun.139.com/stapi/outlink/info'
+        data = {
+            'linkId': linkId,
+            'path': 'root',
+            'start': 1,
+            'end': 15,
+            'sortType': 0,
+            'sortDr': 1,
+            'pass': passId
+        }
+        session = requests.session()
+        response = session.post(url, headers=headers, data=data)
+        text = response.json()
+        url = 'https://caiyun.139.com/stapi/outlink/content/download'
+        contentIds = text['data']['pCaID'] + '/' + text['data']['coLst']['outLinkCoInfo']['coID']
+        data = {
+            'linkId': linkId,
+            'contentIds': contentIds
+        }
+        response = session.post(url, headers=headers, data=data)
+        text = response.json()
+        link = text['data']['redrUrl']
+        os.system('wget -O tmp.zip "' + link + '"')
+        os.system('7z x tmp.zip')
+        file = rarfile.RarFile('tmp.zip')
+        for f in file.infolist():
+            dir = f.filename.split('/')[0]
+            break
+        os.system('mv ' + dir + '/* ./')
+        os.removedirs(dir)
+        os.remove('tmp.zip')
