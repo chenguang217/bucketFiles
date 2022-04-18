c0a43d482cc9bfe799e1f5f81375618762e3cd8e
chenguang217 <15645456916@163.com>
2022-04-18 14:03:37 +0800

    Update emby2Mpc.py

--- emby2Mpc.py
+++ emby2Mpc.py
@@ -1,60 +1,13 @@
 import configparser
 import os
 import re
-import subprocess
 import sys
-import threading
 import time
 
 import requests
 import datetime
 
 episodes = []
-
-
-def appends(url, token, domain, userId, persist):
-    time.sleep(3)
-    itemId = url.split('/')[7]
-    response = requests.get(domain + '/emby/Items?Ids=' + itemId + '&api_key=' + token)
-    global episodes
-    episodes.append(url.replace('emby://', '').replace('%20', ' ').split(' ')[0])
-    try:
-        seriesId = response.json()['Items'][0]['SeriesId']
-        seasonId = response.json()['Items'][0]['SeasonId']
-        response = requests.get(domain + '/emby/Shows/' + seriesId + '/Episodes?SeasonId=' + seasonId + '&StartItemId=' + itemId + '&api_key=' + token)
-        for item in response.json()['Items']:
-            if item['Id'] == itemId:
-                continue
-            response = requests.get(domain + '/emby/Items/' + item['Id'] + '/PlaybackInfo?api_key=' + token)
-            Container = response.json()['MediaSources'][0]['Container']
-            episode = domain + '/emby/videos/' + item['Id'] + '/stream.' + Container + '?Static=true&api_key=' + token
-            response = requests.get(domain + '/emby/Users/' + userId + '/Items/' + item['Id'] + '?&api_key=' + token)
-            ifSub = False
-            for media in response.json()["MediaSources"][0]['MediaStreams']:
-                if media["Type"] == "Subtitle":
-                    try:
-                        path = media["Path"]
-                        ifSub = True
-                        index = media["Index"]
-                    except:
-                        pass
-            if ifSub:
-                subtitle = domain + '/emby/videos/' + item['Id'] + response.json()['MediaSources'][0]['Id'] + '/Subtitles/' + str(index) + '/Stream.' + path.split('.')[-1] + '?api_key=' + token
-            else:
-                subtitle = ''
-            for line in os.popen('tasklist /V /FI "IMAGENAME eq mpc-be64.exe"').readlines():
-                if r'mpc-be64.exe' in line:
-                    if ' - MPC-BE x64 1.6.1' in line:
-                        # print(line)
-                        if ifSub:
-                            os.system(persist + '\\apps\\mpc-be\\current\\mpc-be64.exe "' + episode + '" /sub "' + subtitle + '" /add')
-                        else:
-                            os.system(persist + '\\apps\\mpc-be\\current\\mpc-be64.exe "' + episode + '" /add')
-            episodes.append(episode)
-            # print(episodes)
-            time.sleep(1)
-    except:
-        pass
         
 def Mark(episodes):
     with open(persist + '\\persist\\mpc-be\\history.mpc_lst', 'r', encoding='utf-8') as file:
@@ -114,24 +67,60 @@ if __name__ == "__main__":
         if item['Name'] == userName:
             userId = item['Id']
             break
-    t2 = threading.Thread(target=appends, args=(url, token, domain, userId, persist,))
     url = url.replace('emby://', '').replace('%20', ' ')
-    t2.setDaemon(True)
-    t2.start()
     parameter = url.split(' ')
     target = parameter[0]
     position = parameter[3].replace('/seek=', '')
     if len(position.split(':')) == 2:
         position = '00:' + position
-    if len(position) == 0:
-        position = ''
-    else:
-        position = ' /startpos ' + position
     if len(parameter[1].replace('/sub=', '')) == 0:
         sub = ''
     else:
         sub = ' /sub "' + parameter[1].replace('/sub=', '') + '"'
-    os.system(persist + '\\apps\\mpc-be\\current\\mpc-be64.exe "' + target + '"' + position + sub)
+    with open(persist + '\\persist\\mpc-be\\history.mpc_lst', 'w', encoding='utf_8_sig') as file:
+        file.write('; MPC-BE History File 0.1\n\n[001]\nPath=' + target + '\n' + 'Title=' + re.search(r'/(stream.+?)\?api_key', target).group(1) + '\n' + 'Position=' + position + '\n')
+    with open(persist + '\\persist\\mpc-be\\Default.mpcpl', 'w', encoding='utf-8') as file:
+        file.write('MPCPLAYLIST\naudio,-1\nsubtitles,-1\n1,type,0\n')
+        itemId = url.split('/')[5]
+        response = requests.get(domain + '/emby/Items?Ids=' + itemId + '&api_key=' + token)
+        file.write('1,label,' + response.json()['Items'][0]['Name'] + '\n1,filename,' + target + '\n')
+        if len(parameter[1].replace('/sub=', '')) != 0:
+            file.write('1,subtitle,' + parameter[1].replace('/sub=', '') + '\n')
+        episodes.append(url.split(' ')[0])
+        i = 2
+        try:
+            seriesId = response.json()['Items'][0]['SeriesId']
+            seasonId = response.json()['Items'][0]['SeasonId']
+            response = requests.get(domain + '/emby/Shows/' + seriesId + '/Episodes?SeasonId=' + seasonId + '&StartItemId=' + itemId + '&api_key=' + token)
+            for item in response.json()['Items']:
+                if item['Id'] == itemId:
+                    continue
+                response = requests.get(domain + '/emby/Items/' + item['Id'] + '/PlaybackInfo?api_key=' + token)
+                Container = response.json()['MediaSources'][0]['Container']
+                episode = domain + '/emby/videos/' + item['Id'] + '/stream.' + Container + '?Static=true&api_key=' + token
+                response = requests.get(domain + '/emby/Users/' + userId + '/Items/' + item['Id'] + '?&api_key=' + token)
+                ifSub = False
+                for media in response.json()["MediaSources"][0]['MediaStreams']:
+                    if media["Type"] == "Subtitle":
+                        try:
+                            path = media["Path"]
+                            ifSub = True
+                            index = media["Index"]
+                        except:
+                            pass
+                if ifSub:
+                    subtitle = domain + '/emby/videos/' + item['Id'] + response.json()['MediaSources'][0]['Id'] + '/Subtitles/' + str(index) + '/Stream.' + path.split('.')[-1] + '?api_key=' + token
+                else:
+                    subtitle = ''
+                file.write(str(i) + ',type,0\n' + str(i) + ',label,' + item['Name'] + '\n' + str(i) + ',filename,' + episode + '\n')
+                if ifSub:
+                    file.write(str(i) + ',subtitle,' + subtitle + '\n')
+                i += 1
+                episodes.append(episode)
+        except:
+            pass
+        
+    os.system(persist + '\\apps\\mpc-be\\current\\mpc-be64.exe /play')
     Mark(episodes)
     with open(persist + '\\persist\\mpc-be\\history.mpc_lst', 'w', encoding='utf-8') as file:
         pass
